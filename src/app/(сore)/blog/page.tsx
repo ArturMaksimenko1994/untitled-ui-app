5b32ad91959da09133768cc664872e999031c336
Artur <arturmaksimenko784@gmail.com>
2024-10-28 08:10:49 +0300

    add ArticleNews

--- src/app/(сore)/blog/page.tsx
+++ src/app/(сore)/blog/page.tsx
@@ -1,13 +1,50 @@
 import type { Metadata } from 'next'
 import styles from "./blog.module.scss"
+import ArticleNews from "@/ui/articles/ArticleNews/ArticleNews";
 
 const PageBlog = () => {
+  const urlFake = ""
   return (
-    <div className={styles.page}>
+    <section className={styles.news}>
       <div className="container">
-      идет в разработке
+        <h2 className="h4">Все записи в блоге</h2>
+        <div className={styles.news__row}>
+
+          <ArticleNews
+            link="#"
+            title="PM mental models"
+            description="Mental models are simple expressions of complex processes or relationships."
+            imageSrc="https://www.serconsrus.ru/app/uploads/2024/10/dsc08556-1-1-1200x929.jpg.webp"
+            alt="Новость"
+            author="Demi Wilkinson"
+            date="16 Jan 2025"
+            tags={["Product"]}
+          />
+
+          <ArticleNews
+            link="#"
+            title="PM mental models"
+            description="Mental models are simple expressions of complex processes or relationships."
+            imageSrc="https://www.serconsrus.ru/app/uploads/2024/10/dsc08556-1-1-1200x929.jpg.webp"
+            alt="Новость"
+            author="Demi Wilkinson"
+            date="16 Jan 2025"
+            tags={["Product"]}
+          />
+
+          <ArticleNews
+            link="#"
+            title="PM mental models"
+            description="Mental models are simple expressions of complex processes or relationships."
+            imageSrc="https://www.serconsrus.ru/app/uploads/2024/10/dsc08556-1-1-1200x929.jpg.webp"
+            alt="Новость"
+            author="Demi Wilkinson"
+            date="16 Jan 2025"
+            tags={["Product"]}
+          />
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/app/(сore)/page.tsx
+++ src/app/(сore)/page.tsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
-
+import styles from "./page.module.scss"
 const PageHome = () => {
   return (
     <>
-      <h1 className="h1">Главная страница</h1>
-      <Link href="/dashboard">
-        страница dashboard
-      </Link>
+      <section className={styles.hero}>
+        <div className="container">
+          <h1 className="h1">Главная страница</h1>
+          <Link href="/dashboard">
+            страница dashboard
+          </Link>
+        </div>
+      </section>
     </>
   );
 };
