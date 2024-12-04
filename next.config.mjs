c37e11c451ff8748df5b58a7f4dfd5e467229311
Martin Schulz <martinschulzofficial@gmail.com>
2024-12-04 13:03:11 +0500

    build: add global sass variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['www.serconsrus.ru'], // Добавьте сюда нужный домен
-    },
+  images: {
+    domains: ["www.serconsrus.ru"], // Добавьте сюда нужный домен
+  },
+  sassOptions: {
+    includePaths: ["./src/styles/abstracts"],
+    prependData: `@import "index.scss";`,
+  },
 };
 
 export default nextConfig;
