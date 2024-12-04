ee564373a37d63c81b423938675693bdae0c0bc7
Artur <arturmaksimenko784@gmail.com>
2024-12-04 11:40:22 +0300

    refactor

--- next.config.ts
+++ next.config.ts
@@ -1,54 +1,12 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   css: {
-//     preprocessorOptions: {
-//       scss: {
-//         additionalData: `
-//           @import "./src/styles/abstracts/_variables.scss";
-//           @import "./src/styles/abstracts/_media-queries.scss";
-//         `,
-//       },
-//     },
-//   },
-//   // sassOptions: {
-//   //   additionalData: `
-//   //       @import "./src/assets/scss/_mixins.scss";
-//   //       @import "./src/assets/scss/_variables.scss";
-//   //     `,
-//   // },
-// };
-
-// import path from "path";
-
-
-// import type { NextConfig } from 'next'
-
-// const nextConfig = {
-//   images: {
-//     domains: ['untitled-ui-api.ru'],
-//   },
-//   options: {
-//     additionalData: `
-//       @import "${path.resolve(__dirname, 'src/styles/abstracts/_variables.scss')}";
-//       @import "${path.resolve(__dirname, 'src/styles/abstracts/_media-queries.scss')}";
-//     `,
-//   },
-// };
-
-// export default nextConfig;
-
-const path = require('path');
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["www.serconsrus.ru"], // Добавьте сюда нужный домен
+  },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
-    additionalData: `
-      @use "abstracts/animations" as *;
-      @use "abstracts/mixins" as *;
-      @use "abstracts/media-queries" as *;
-      @use "abstracts/variables" as *;
-    `,
+    includePaths: ["./src/styles/abstracts"],
+    prependData: `@import "index.scss";`,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
